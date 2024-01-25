10b961262dcead7ec02c404c054e2c51796fe04d
SerenaPang4 <ypang4@usfca.edu>
2024-01-24 16:59:44 -0800

    test for red black tree

---
+++ javabasic/src/org/bigjava/ch17/treestructures/PermutationGenerator.java
@@ -0,0 +1,14 @@
+package org.bigjava.ch17.treestructures;
+/**
+ * This class generates permutations of a word
+ * */
+public class PermutationGenerator {
+	private String word;
+	/**
+	 * constructs a permutation generator
+	 * @param aWord the word to permute
+	 * */
+	public PermutationGenerator(String aWord) {
+		word = aWord;
+	}
+}
---
+++ javabasic/src/org/bigjava/ch17/treestructures/RedBlackTreeTester.java
@@ -0,0 +1,63 @@
+package org.bigjava.ch17.treestructures;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This program tests the red black tree class
+ * */
+public class RedBlackTreeTester {
+	public static void main(String[] args) {
+		testFromBook();
+		insertionTest("ABCDEFGHIJ");
+		System.out.println("All tests passed.");
+	}
+
+	private static void removelTest(Object removalTestTemplate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static Object removalTestTemplate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * inserts all permutations of a string into a red - black tree and checks that it contains the string afterwards
+	 * @param letters a string of letters without repetition
+	 * */
+	private static void insertionTest(String letters) {
+		PermutationGenerator gen = new PermutationGenerator(letters);
+		
+	}
+	/**
+	 * runs simple test from the textbook
+	 * */
+	private static void testFromBook() {
+		RedBlackTree t = new RedBlackTree();
+		t.add("D");
+		t.add("B");
+		t.add("A");
+		t.add("C");	
+		t.add("F");
+		t.add("E");
+		t.add("I");
+		t.add("G");
+		t.add("H");
+		t.add("J"); 
+		
+		t.remove("A");//removing leaf
+		t.remove("B");//removing element with one child
+		t.remove("F");// removing element with two children
+		t.remove("D");//removing root
+		assertEquals("C E G H I J", t.toString());
+	}
+
+	private static void assertEquals(Object expected, Object actual) {
+		if(expected == null && actual != null || !expected.equals(actual)) {
+			if (expected == null && actual != null || !expected.equals(actual)) {
+				throw new AssertionError("Expected " + expected + "but found " + actual);
+			}
+		}		
+	}
+}
