b8c0937aa1fc95ddc703950be7e1196dc344372a
SerenaPang4 <ypang4@usfca.edu>
2024-01-29 12:48:09 -0800

    application of stack and queue and priority queue

--- javabasic/src/org/bigjava/ch15/javacollectionsframework/QueueDemo.java
+++ javabasic/src/org/bigjava/ch15/javacollectionsframework/QueueDemo.java
@@ -9,5 +9,17 @@ import java.util.Queue;
 public class QueueDemo {
 	public static void main() {
 		Queue<String> jobs = new LinkedList<>();
+		jobs.add("Joe: Expense Report #1");
+		jobs.add("Cathy: Meeting Memo");
+		System.out.println("Printing " + jobs.remove());
+		jobs.add("Cathy: Purchase Order #1");
+		jobs.add("Joe: Expense Report #2");
+		jobs.add("Joe: Weekly Report");
+		System.out.println("Printing " + jobs.remove());
+		jobs.add("Cathy: Purchase Order #2");
+		
+		while (jobs.size() > 0) {
+			System.out.println(" " + jobs.remove());
+		}
 	}
 }
