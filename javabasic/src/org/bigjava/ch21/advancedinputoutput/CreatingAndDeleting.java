cc9307d19592a38b4ccd8e0598ae93b96acaada1
SerenaPang4 <ypang4@usfca.edu>
2024-02-01 13:58:17 -0800

    creating and deleting files

--- javabasic/src/org/bigjava/ch21/advancedinputoutput/CreatingAndDeleting.java
+++ javabasic/src/org/bigjava/ch21/advancedinputoutput/CreatingAndDeleting.java
@@ -1,9 +1,33 @@
 package org.bigjava.ch21.advancedinputoutput;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CreatingAndDeleting {
 	public static void main(String[] args) throws IOException{
-		//creating a directory and a file
+		//creating a directory and a file inside it
+		Files.createDirectories(Paths.get("subdir"));
+		Path testPath = Paths.get("subdir/test.txt");
+		Files.createFile(testPath);
+		
+		//checking that the file exists
+		System.out.println(testPath + " exists: " + Files.exists(testPath));
+		//but this one doesn't exist
+		Path badPath = Paths.get("subdir/bad");
+		System.out.println(badPath + " exists: " + Files.exists(badPath));
+		
+		//confirming that it's a file and not a directory
+		System.out.println(testPath + " is a directory: " + Files.isRegularFile(testPath));
+		
+		//deleting what was created
+		Files.delete(testPath);
+		Files.delete(testPath.getParent());
+		
+		Path tempFile = Files.createTempFile("bigjava", ".txt");
+		System.out.println("Creating a temporary file " + tempFile);
+		Path tempDir = Files.createTempDirectory("bigjava");
+		System.out.println("Created a temporary directory " + tempDir);
 	}
 }
