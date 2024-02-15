a147286d9047769fefe2f747be21f2004e720837
SerenaPang4 <ypang4@usfca.edu>
2024-02-15 15:44:34 -0800

    updates

---
+++ javabasic/src/org/bigjava/ch24/rationaldatabases/BankAccount.java
@@ -0,0 +1,11 @@
+package org.bigjava.ch24.rationaldatabases;
+
+public class BankAccount {
+	/**
+	 * A bank account has a balance that can be changed by deposits and withdrawals
+	 * */
+	public BankAccount(int checkingAccountNumber) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
