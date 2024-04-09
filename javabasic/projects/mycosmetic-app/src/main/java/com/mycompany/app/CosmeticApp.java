91e94a5fbc8b81db4ad169deb89e456592e12c1d
SerenaPang4 <ypang4@usfca.edu>
2024-04-08 21:33:47 -0700

    Add flags -db and -file to decide where to store data

--- javabasic/projects/mycosmetic-app/src/main/java/com/mycompany/app/CosmeticApp.java
+++ javabasic/projects/mycosmetic-app/src/main/java/com/mycompany/app/CosmeticApp.java
@@ -1,51 +1,42 @@
 package com.mycompany.app;
+
 import java.io.File;
+import java.sql.Connection;
 
 /**
- * The app to save and search for the cosmetic product information from a text file
- * run the application with this command if not using database:
- * java -cp target/mycosmetic-app-1.0-SNAPSHOT.jar com.mycompany.app.CosmeticApp /Users/serenapang/Development/JavaBasics/javabasic/projects/mycosmetic-app/cosmetic.txt
+ * The app to save and search for the cosmetic product information from a text
+ * file run the application with this command if not using database: java -cp
+ * target/mycosmetic-app-1.0-SNAPSHOT.jar com.mycompany.app.CosmeticApp
+ * /Users/serenapang/Development/JavaBasics/javabasic/projects/mycosmetic-app/cosmetic.txt
  * 
- * run with database 
+ * run with database
  * 
- * java -cp target/mycosmetic-app-1.0-SNAPSHOT.jar com.mycompany.app.CosmeticApp /Users/serenapang/Development/JavaBasics/javabasic/projects/mycosmetic-app/cosmetic.txt database
  * 
+ * - delete the target jar file in github - save the entries from the user to
+ * database - search the result in the database for the user - elaborate read me
  */
-public class CosmeticApp 
-{
-//    public static void main( String[] args) {
-//    	//if flag is true, we use the database to store info
-//    	boolean flag = true;
-//    	   	
-//    	//take the command line arguments which is the file path, and the flag to indictae if database is used
-//    	if (args.length == 1) {
-//    		
-//    		//System.out.println(args[0]);
-//    		if (args[0].endsWith("txt")) {
-//    			flag = false;
-//    		} else {
-//    			flag = true;
-//    		}
-//    		
-//    		//choose to use text file or database
-//    		if (!flag) {
-//    			System.out.println("Use text file");
-//    			File file = new File(args[0]);
-//        		System.out.println("Found text file name: " + file.getName());
-//        		System.out.println("Path: " + file.getAbsolutePath());
-//        		pass the file to File processor
-//        		GridLayoutFrame ui = new GridLayoutFrame(file);
-//            	ui.createUi();
-//    		} else {
-//    			System.out.println("Use database");
-//    			//System.out.println(args[0]);
-//    			System.out.println("Usage: java -classpath /Users/serenapang/Downloads/db-derby-10.15.2.0-bin/lib" + File.pathSeparator + ".TestCosmeticDB propertiesFile");
-//    			return;
-//    		}
-//    		 		
-//    		
-//    	}else {
-//    		System.out.println("Please pass the path of the file or as an argument");
-//    	}
-//    }
+public class CosmeticApp {
+	public static void main(String[] args) {
+		// if flag is true, we use the database to store info
+		for (String s : args) {
+			System.out.println("Parameter " + s);
+		}
+
+		if (args.length < 1) {
+			System.out.println("One parameter is required <-db|-file>");
+			return;
+		}
+
+		String flag = args[0];
+
+		if ("-file".equals(flag)) {
+			System.out.println("Use text file");
+			File file = new File(
+					"/Users/serenapang/Development/JavaBasics/javabasic/projects/mycosmetic-app/cosmetic.txt");
+			GridLayoutFrame ui = new GridLayoutFrame(file);
+			ui.createUi();
+		} else {
+			System.out.println("Use database");
+		}
+	}
 }
