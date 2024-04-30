88d3f21116e579789a78f3da365d334af02fa0d2
SerenaPang4 <ypang4@usfca.edu>
2024-04-30 15:34:04 -0700

    complete update method for random aceess file example

--- javabasic/src/org/corejava/ch9/processinginputoutput/DataDao.java
+++ javabasic/src/org/corejava/ch9/processinginputoutput/DataDao.java
@@ -136,7 +136,50 @@ public class DataDao {
 	 * to find the matching id replace the line with the new one
 	 */
 	public void update(Data data) {
+		//new data entry id to be updated
+		int dataId = data.getId();
+		
+		try (RandomAccessFile randomFile = new RandomAccessFile(pathFile, "rw")) {
+			long fileSize = randomFile.length();
+			System.out.println("start reading file");
 
+			int pointer = 0;
+			
+			// when it's the end of the file stop, pointer >= size of file
+			// set the pointer +16 every time read a line, move the pointer by 4, 8, and 4, update pointer position for every iteration
+			// to get the data, create the data object and add to list
+			while (pointer < fileSize) {
+				randomFile.seek(pointer);
+				int currentId = randomFile.readInt();
+				pointer = pointer + 4;
+				randomFile.seek(pointer);
+				long zip = randomFile.readLong();
+				pointer = pointer + 8;
+				randomFile.seek(pointer);
+				int num = randomFile.readInt();
+				pointer = pointer + 4;
+				//found the matching id, move the pointer to each location, write the new info
+				if (currentId == dataId) {
+					System.out.println("found id " + dataId);
+//					move pointer backward to update num
+					pointer = pointer - 4;		
+					randomFile.seek(pointer);
+					randomFile.writeInt(data.getNum());
+					//move pointer backward to /update zip
+					pointer = pointer - 8;
+					randomFile.seek(pointer);
+					randomFile.writeLong(data.getZip());
+					break;
+				}	
+			}
+			readAll();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to open the file " + pathFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to access " + pathFile);
+		}
 	}
 
 	/**
@@ -148,11 +191,11 @@ public class DataDao {
 	}
 
 	public static void main(String args[]) {
-		Data d1 = new Data(1, 2, 3);
+		Data d1 = new Data(18, 23547768, 300);
 //		Data d2 = new Data(2, 94102, 111);
 //		Data d3 = new Data(3, 94103, 222);
 //		Data d4 = new Data(4, 94104, 333);
-//		Data d5 = new Data(5, 94105, 444);
+		Data d5 = new Data(5, 94105555, 5555);
 //		Data d6 = new Data(6, 94106, 555);
 //		Data d7 = new Data(7, 94107, 666);
 //		Data d8 = new Data(8, 94108, 777);
@@ -162,10 +205,11 @@ public class DataDao {
 		String path = "/Users/serenapang/Development/JavaBasics/javabasic/"
 				+ "src/org/corejava/ch9/processinginputoutput/myrandomdata.txt";
 		DataDao datadao = new DataDao(path);
-		// datadao.save(d1);
+		//datadao.save(d5);
 		//datadao.readAll();
 		// System.out.println("Finish saving");
 		// datadao.save(d2);
-		datadao.findById(18);
+		//datadao.findById(18);
+		datadao.update(d5);
 	}
 }
