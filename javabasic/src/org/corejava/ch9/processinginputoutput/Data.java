f78da8646a68f56e70ec3d52e7727097af0f9d97
SerenaPang4 <ypang4@usfca.edu>
2024-05-02 14:51:20 -0700

    compelete add method with String example

--- javabasic/src/org/corejava/ch9/processinginputoutput/Data.java
+++ javabasic/src/org/corejava/ch9/processinginputoutput/Data.java
@@ -4,13 +4,19 @@ public class Data {
 	private int id; // 4 bytes
 	private long zip;// 8 bytes
 	private int num;// 4 bytes
-//	String name;//4 bytes
+	String name;//4 bytes
 
 	public Data(int id, long zip, int num) {
 		this.id = id;
 		this.zip = zip;
 		this.num = num;
-		// this.name = name;
+	}
+	
+	public Data(int id, long zip, int num, String name) {
+		this.id = id;
+		this.zip = zip;
+		this.num = num;
+	    this.name = name;
 	}
 
 	public int getNum() {
@@ -37,17 +43,17 @@ public class Data {
 		this.zip = zip;
 	}
 
-//	public String getName() {
-//		return name;
-//	}
-//	
-//	public void setName(String name) {
-//		this.name = name;
-//	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	@Override
 	public String toString() {
-		// return "Data [id=" + id + ", zip=" + zip + ", name=" + name + "]";
-		return "Data [id=" + id + ", zip=" + zip + ", num=" + num + "]";
+		 return "Data [id=" + id + ", zip=" + zip + " num=" + num + "]" + " name=" + name + "]";
+		//return "Data [id=" + id + ", zip=" + zip + ", num=" + num + "]";
 	}
 }
--- javabasic/src/org/corejava/ch9/processinginputoutput/DataDao.java
+++ javabasic/src/org/corejava/ch9/processinginputoutput/DataDao.java
@@ -41,6 +41,63 @@ public class DataDao {
 	}
 
 	/**
+	 * store the data with String in the text file
+	 */
+	public void add(Data data) {
+		// create a new RandomAccessFile with filename test "c:/test.txt"
+		try (RandomAccessFile randomFile = new RandomAccessFile(pathFile, "rw")) {
+			System.out.println("Initial file size: " + randomFile.length() + " bytes");
+
+			// Set the pointer till the end.
+			randomFile.seek(randomFile.length());
+
+			randomFile.writeInt(data.getId());
+			randomFile.writeLong(data.getZip());
+			randomFile.writeInt(data.getNum());
+
+			// only allow max 20 characters, 20 * 2 bytes = 40 bytes
+			// store the string in byte array, write it to file, put the unfilled bytes to 0
+			String name = data.getName();
+
+			// fill the byte array with the right length and content
+			char[] strArray = name.toCharArray();
+			char[] byteArr = new char[20];
+			if (strArray.length <= byteArr.length) {
+				// string length <= set array length
+				for (int i = 0; i < strArray.length; i++) {
+					byteArr[i] = strArray[i];
+					// System.out.print(strArray[i] + " ");
+				}
+				for (int j = strArray.length; j < byteArr.length; j++) {
+					byteArr[j] = 'X';
+					// System.out.print(strArray[i] + " ");
+				}
+			} else { // string length > set array length
+				for (int i = 0; i < byteArr.length; i++) {
+					byteArr[i] = strArray[i];
+				}
+			}
+//			read array content
+//			for (int i = 0; i < byteArr.length; i++) {
+//				System.out.println(i + " " + byteArr[i] + " ");
+//			}
+
+			// write each character to the file
+			for (int i = 0; i < byteArr.length; i++) {
+				randomFile.writeChar(byteArr[i]);
+				// System.out.println(i + " " + byteArr[i] + " ");
+			}
+			System.out.println("Final file size: " + randomFile.length() + " bytes");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to open the file " + pathFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to access " + pathFile);
+		}
+	}
+
+	/**
 	 * find all the data in the text file read each line, output to each field, form
 	 * a data object add the object to list
 	 */
@@ -58,6 +115,40 @@ public class DataDao {
 				int id = randomFile.readInt();
 				long zip = randomFile.readLong();
 				int num = randomFile.readInt();
+				end = end + 16;
+				System.out.println("id: " + id + "    zip: " + zip + "    num: " + num);
+				Data data = new Data(id, zip, num);
+				listOfData.add(data);
+			}
+			System.out.println("done reading file");
+			// printList(listOfData);
+			// System.out.println("Final file size: " + randomFile.length() + " bytes");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to open the file " + pathFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to access " + pathFile);
+		}
+		return listOfData;
+	}
+
+	/**
+	 * find all the data in the text file read each line, output to each field, form
+	 * a data object add the object to list
+	 */
+	public List<Data> findAll() {
+		try (RandomAccessFile randomFile = new RandomAccessFile(pathFile, "rw")) {
+			System.out.println("Initial file size: " + randomFile.length() + " bytes");
+			long fileSize = randomFile.length();
+			System.out.println("start reading file");
+			int end = 0;
+
+			while (end < fileSize) {
+				int id = randomFile.readInt();
+				long zip = randomFile.readLong();
+				int num = randomFile.readInt();
+				// char name = randomFile.readChar();
 				end = end + 16;
 				System.out.println("id: " + id + "    zip: " + zip + "    num: " + num);
 				Data data = new Data(id, zip, num);
@@ -210,14 +301,15 @@ public class DataDao {
 			{
 				System.out.println("...");
 				while (end < fileSize) {
-					//update the pointer for each copy iteration so we don't copy the same thing we just did
+					// update the pointer for each copy iteration so we don't copy the same thing we
+					// just did
 					randomFile.seek(pointer);
 					// read the next data entry
 					int nxtId = randomFile.readInt();
 					long nxtZip = randomFile.readLong();
 					int nxtNum = randomFile.readInt();
 					pointer = pointer + 16;
-					
+
 					randomFile.seek(expectedLocation);
 					randomFile.writeInt(nxtId);
 					randomFile.writeLong(nxtZip);
@@ -240,12 +332,13 @@ public class DataDao {
 		Data d2 = new Data(2, 94102, 111);
 		Data d3 = new Data(3, 94103, 222);
 		Data d4 = new Data(4, 94104, 333);
-		Data d5 = new Data(5, 94105555, 5555);
-		Data d6 = new Data(6, 94106, 555);
-		Data d7 = new Data(7, 94107, 666);
-		Data d8 = new Data(8, 94108, 777);
-		Data d9 = new Data(9, 94109, 888);
-		Data d10 = new Data(10, 2, 3);
+
+		Data d5 = new Data(5, 94105555, 5555, "Silver ");
+		Data d6 = new Data(6, 94106, 555, "Diana");
+		Data d7 = new Data(7, 94107, 666, "Tiramisu");
+		Data d8 = new Data(8, 94108, 777, "Tres Leches");
+		Data d9 = new Data(9, 94109, 888, "Coconut cream");
+		Data d10 = new Data(10, 2, 3, "Dark Chocalate");
 
 		String path = "/Users/serenapang/Development/JavaBasics/javabasic/"
 				+ "src/org/corejava/ch9/processinginputoutput/myrandomdata.txt";
@@ -254,13 +347,13 @@ public class DataDao {
 //		datadao.save(d2);
 //		datadao.save(d3);
 //		datadao.save(d4);
-//		datadao.save(d5);
-//
-//		datadao.save(d6);
-//		datadao.save(d7);
-//		datadao.save(d8);
-//		datadao.save(d9);
-//		datadao.save(d10);
+
+		datadao.add(d5);
+//		datadao.add(d6);
+//		datadao.add(d7);
+//		datadao.add(d8);
+//		datadao.add(d9);
+//		datadao.add(d10);
 
 		// System.out.println("Before: ");
 		// datadao.readAll();
@@ -268,8 +361,9 @@ public class DataDao {
 		// datadao.findById(0);
 //		datadao.update(d1);
 //		datadao.readAll();
-		datadao.delete(d2);
+//		datadao.delete(d2);
 //		System.out.println("After: ");
-		datadao.readAll();
+
+		// datadao.readAll();
 	}
 }
