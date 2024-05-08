45ad248ecc51d6e0b5e9150a9b993d26bb8af80a
SerenaPang4 <ypang4@usfca.edu>
2024-05-08 15:19:58 -0700

    compelete save method for RandomAccessFileDao in CosmeticApp

--- javabasic/projects/mycosmetic-app/src/main/java/com/mycompany/app/CosmeticApp.java
+++ javabasic/projects/mycosmetic-app/src/main/java/com/mycompany/app/CosmeticApp.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mycompany.dao.CosmeticDao;
 import com.mycompany.dao.jdbc.CosmeticJdbcDao;
@@ -13,10 +15,11 @@ import com.mycompany.dao.file.CosmeticRandomAccessFileDao;
 import com.mycompany.model.Cosmetic;
 
 /**
- * The app to save and search for the cosmetic product information from a textfile / database
- * run the application with this command if not using database: 
- java -classpath ./target/mycosmetic-app-1.0-SNAPSHOT-jar-with-dependencies.jar \
-    com.mycompany.app.CosmeticApp -db  or  -file
+ * The app to save and search for the cosmetic product information from a
+ * textfile / database run the application with this command if not using
+ * database: java -classpath
+ * ./target/mycosmetic-app-1.0-SNAPSHOT-jar-with-dependencies.jar \
+ * com.mycompany.app.CosmeticApp -db or -file
  */
 public class CosmeticApp {
 	public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
@@ -37,34 +40,63 @@ public class CosmeticApp {
 			System.out.println("Use text file");
 			File file = new File(
 					"/Users/serenapang/Development/JavaBasics/javabasic/projects/mycosmetic-app/cosmetic.txt");
-	//		cosmeticDao = new CosmeticFileDao(file);
+			cosmeticDao = new CosmeticFileDao(file);
 
-	//		Cosmetic cosme = new Cosmetic(3, "coco", "chanel","perfume");
-	//		cosmeticDao.updateCosmetic(cosme);
-	//		cosmeticDao.deleteCosmetic(4);
-			//String filePath = file.getAbsolutePath();
-			//System.out.println(filePath);
+			// Cosmetic cosme = new Cosmetic(3, "coco", "chanel","perfume");
+			// cosmeticDao.updateCosmetic(cosme);
+			// cosmeticDao.deleteCosmetic(4);
+			// String filePath = file.getAbsolutePath();
+			// System.out.println(filePath);
+
+		} else if("-rafile".equals(flag)) { // rafile stands for random access file
+			File file = new File(
+					"/Users/serenapang/Development/JavaBasics/javabasic/projects/mycosmetic-app/randomcosmetic.txt");
 			cosmeticDao = new CosmeticRandomAccessFileDao(file);
-			//String line = new String("4:Flora:Chloe:Perfume");
-			//String line = new String("hi");
-			//System.out.println(line);
-			Cosmetic cosme = new Cosmetic(4, "Flora", "Chloe","Perfume");
-			//System.out.println(cosme.toString());
-			//cosmeticDao.save(cosme);
-			//cosmeticDao.findAll();
-			cosmeticDao.findById(4);
-		} else {
+
+			System.out.println("Using random access file");
+			Cosmetic d1 = new Cosmetic(1, "aa", "aaaa", "BabyCat 1");
+			Cosmetic d2 = new Cosmetic(2, "bb", "bbbbb", "BabyCat 2");
+			Cosmetic d3 = new Cosmetic(3, "cc", "ccccc", "BabyCat 3");
+			Cosmetic d4 = new Cosmetic(4, "dd", "ddddd", "BabyCat 4");
+
+			Cosmetic d5 = new Cosmetic(5, "ee", "eee", "BabyCat");
+			Cosmetic d6 = new Cosmetic(6, "ff", "ffff", "Hello Good night");
+			Cosmetic d7 = new Cosmetic(7, "gg", "ggg", "Tiramisu");
+			Cosmetic d8 = new Cosmetic(8, "hh", "hhhh", "Tres Leches");
+			Cosmetic d9 = new Cosmetic(9, "ii", "iii", "Coconut cream");
+			Cosmetic d10 = new Cosmetic(10, "jjjj", "jjjj", "Dark C");
+//			 cosmeticDao.save(d1);
+//			 cosmeticDao.save(d2);
+//			 cosmeticDao.save(d3);
+//			 cosmeticDao.save(d4);
+//			 cosmeticDao.save(d5);
+//			 cosmeticDao.save(d6);
+//			 cosmeticDao.save(d7);
+//			 cosmeticDao.save(d8);
+//			 cosmeticDao.save(d9);
+//			 cosmeticDao.save(d10);
+			 
+			 List<Cosmetic> listOfCosmetic = cosmeticDao.findAll();
+			 for (Cosmetic c : listOfCosmetic) {
+					System.out.println(c.toString());
+			 }
+			 
+			// cosmeticDao.findById(4);
+		}
+		 else if  ("-db".equals(flag)){
 			System.out.println("Use database");
-			JdbcDataSource jdbcDataSource = 
-					new JdbcDataSource("mysqldb.properties");
+			JdbcDataSource jdbcDataSource = new JdbcDataSource("mysqldb.properties");
 			cosmeticDao = new CosmeticJdbcDao(jdbcDataSource);
-			//		Cosmetic cosme = new Cosmetic(3, "coco", "chanel","perfume");
+			// Cosmetic cosme = new Cosmetic(3, "coco", "chanel","perfume");
 //					Cosmetic cosme = new Cosmetic(1, "aa", "cc","aa");
 //					cosmeticDao.updateCosmetic(cosme);
-			//cosmeticDao.deleteCosmetic(90);
-			//cosmeticDao.findById(4);
+			// cosmeticDao.deleteCosmetic(90);
+			// cosmeticDao.findById(4);
 		}
-//		GridLayoutFrame ui = new GridLayoutFrame(cosmeticDao, flag);
-//		ui.createUi();
+			 else {
+				 //serilization
+			 }			 
+//				GridLayoutFrame ui = new GridLayoutFrame(cosmeticDao, flag);
+//				ui.createUi(); 
 	}
 }
--- javabasic/projects/mycosmetic-app/src/main/java/com/mycompany/dao/file/CosmeticRandomAccessFileDao.java
+++ javabasic/projects/mycosmetic-app/src/main/java/com/mycompany/dao/file/CosmeticRandomAccessFileDao.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,79 +15,84 @@ import com.mycompany.model.Cosmetic;
 public class CosmeticRandomAccessFileDao implements CosmeticDao {
 	// create a map to store all the product, the key is the product id
 	Map<Integer, Cosmetic> productMap = new HashMap<Integer, Cosmetic>();
+	private List<Cosmetic> listOfCosmetic = new ArrayList<>();
 
 	// create a file object
 	File file;
+//	String pathFile;
+
+	private static final int CHAR_SIZE_IN_BYTES = 2;
+	private static final int ID_FIELD_SIZE_IN_BYTES = 4;
+	private static final int STRING_FIELD_SIZE_IN_BYTES = 20 * 2; // Every character is 2 bytes.
+	private static final int STRING_FIELD_SIZE_IN_CHARS = 20; // Every character is 2 bytes.
+	/*
+	 * The record size for the data record {@code Data}.
+	 */
+	private static final int DATA_RECORD_SIZE_IN_BYTES = ID_FIELD_SIZE_IN_BYTES + STRING_FIELD_SIZE_IN_BYTES
+			+ STRING_FIELD_SIZE_IN_BYTES + STRING_FIELD_SIZE_IN_BYTES;
 
 	// create a constructor that receive the file pass in the command line
 	public CosmeticRandomAccessFileDao(File file) {
 		this.file = file;
 	}
 
+//	public CosmeticRandomAccessFileDao(String pathFile) {
+//		this.pathFile = pathFile;
+//	}
+
 	/**
 	 * This method writes the user input into the text at the end of the text file
 	 * using RandomAccessFile
 	 * 
-	 * @param filePath the file to write
-	 * @param line     content to write into the file
+	 * @param cosmetic the cosmetic object to write
 	 */
 	public void save(Cosmetic cosmetic) {
-		int numId = cosmetic.getId();
-		String id = String.valueOf(numId);
-		String name = cosmetic.getName();
-		String brand = cosmetic.getBrand();
-		String category = cosmetic.getCategory();
-
-		String line = new StringBuffer().append(id).append(":").append(name).append(":").append(brand).append(":")
-				.append(category).toString();
-		try {
-			RandomAccessFile raFile = new RandomAccessFile(file.getAbsolutePath(), "rw");
-			raFile.seek(raFile.length());
-			System.out.println("current pointer = " + raFile.getFilePointer());
-			raFile.write(line.getBytes());
-			raFile.close();
-			System.out.println("Data successfully appended at the end of file");
-
-		} catch (IOException ioe) {
-			System.out.println("Exception occur opening file:");
-			ioe.printStackTrace();
+		try (RandomAccessFile randomFile = new RandomAccessFile(file, "rw")) {
+			System.out.println("Initial file size: " + randomFile.length() + " bytes");
+			// Set the pointer till the end.
+			randomFile.seek(randomFile.length());
+			randomFile.writeInt(cosmetic.getId());
+			writeString(cosmetic.getName(), randomFile);
+			writeString(cosmetic.getBrand(), randomFile);
+			writeString(cosmetic.getCategory(), randomFile);
+			System.out.println("Final file size: " + randomFile.length() + " bytes");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to open the file " + file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to access " + file);
 		}
-		// System.out.println(line);
 	}
 
 	public Cosmetic findById(Integer id) {
 		System.out.println("Finding id " + id + " in text file...");
-		try (RandomAccessFile reader = new RandomAccessFile(file, "r")) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				String[] lineList = line.split(":");
-				int currentId = Integer.parseInt(lineList[0]);
-				if (id == currentId) {
-					Cosmetic cosme = new Cosmetic(currentId, lineList[1], lineList[2], lineList[3]);
-					System.out.println(cosme.toString());
-					return cosme;
-				}
-			}
-		} catch (FileNotFoundException fnfe) {
-		} catch (IOException ioe) {
-			System.err.println(ioe);
-		}
 		return null;
 	}
 
 	public List<Cosmetic> findAll() {
-		System.out.println("reading file...");
-		try (RandomAccessFile reader = new RandomAccessFile(file, "r")) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
+		try (RandomAccessFile randomFile = new RandomAccessFile(file, "rw")) {
+			long fileSize = randomFile.length();
+			System.out.println("start reading file");
+			int end = 0;
+			while (end < fileSize) {
+				int id = randomFile.readInt();			
+				String name = readString(randomFile);
+				String brand = readString(randomFile);
+				String category = readString(randomFile);			
+				Cosmetic cosmetic = new Cosmetic(id, name, brand, category);
+				listOfCosmetic.add(cosmetic);
+				end = end + DATA_RECORD_SIZE_IN_BYTES;
 			}
-			System.out.println();
-		} catch (FileNotFoundException fnfe) {
-		} catch (IOException ioe) {
-			System.err.println(ioe);
+			System.out.println("done reading file");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to open the file " + file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to access " + file);
 		}
-		return null;
+		return listOfCosmetic;
 	}
 
 	public boolean updateCosmetic(Cosmetic cosmetic) {
@@ -99,5 +105,76 @@ public class CosmeticRandomAccessFileDao implements CosmeticDao {
 		System.out.println("delete cosmetics");
 
 		return false;
+	}
+
+	/**
+	 * Write a string in the text file
+	 * 
+	 * @param startingPosition where to start to write the characters in file
+	 * @param aString          the String to be written
+	 * @param expectedLength   is the length of the bytes we want to write in file
+	 *                         for the String
+	 * @return the written string's ending position in file
+	 */
+	private void writeString(String aString, RandomAccessFile randomFile) {
+		char[] stringArray = aString.toCharArray();
+		int totalBytes = stringArray.length * CHAR_SIZE_IN_BYTES;
+
+		int bytesToSave = totalBytes <= STRING_FIELD_SIZE_IN_BYTES ? totalBytes : STRING_FIELD_SIZE_IN_BYTES;
+		int charsToSave = bytesToSave / CHAR_SIZE_IN_BYTES;
+
+		int index = 0;
+		for (index = 0; index < charsToSave; index++) {
+			try {
+				randomFile.writeChar(stringArray[index]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		for (int i = STRING_FIELD_SIZE_IN_CHARS - charsToSave; i > 0; i--) {
+			try {
+				randomFile.writeChar(0 /* NULL in ASCII */);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Read a string from the text file
+	 * 
+	 * @param startPoint where to start to read characters in file
+	 * @return the String starting from the pass in position in file
+	 */
+	private String readString(RandomAccessFile randomFile) {
+		String name = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			// read string
+			int strLen = 0;
+			for (int i = 0; i < STRING_FIELD_SIZE_IN_CHARS; i++) {
+				char character = randomFile.readChar();
+				if (character != 0) {
+					strLen++;
+					sb.append(character);
+				}
+			}
+			// convert to string
+			String wholeStr = sb.toString();
+			name = wholeStr.substring(0, strLen);
+			return name;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return name;
+	}
+	
+	/**
+	 * print the list of Cosmetic object
+	 */
+	private void printList(List<Cosmetic> listOfCosmetic) {
+		for (Cosmetic c : listOfCosmetic) {
+			System.out.println(c.toString());
+		}
 	}
 }
