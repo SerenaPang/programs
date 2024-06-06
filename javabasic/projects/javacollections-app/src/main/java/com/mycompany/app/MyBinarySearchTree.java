87d6a42137bcf71aa3f5eda946c9eb3556bdbbe6
SerenaPang4 <ypang4@usfca.edu>
2024-06-05 17:38:13 -0700

    complete JUnit test for BST insert and search method

--- javabasic/projects/javacollections-app/src/main/java/com/mycompany/app/MyBinarySearchTree.java
+++ javabasic/projects/javacollections-app/src/main/java/com/mycompany/app/MyBinarySearchTree.java
@@ -3,7 +3,6 @@ package com.mycompany.app;
 /**
  * Implementation of a Binary Search Tree and its operations (iterative)
  */
-
 public class MyBinarySearchTree {
 	private TreeNode root;
 
@@ -15,28 +14,28 @@ public class MyBinarySearchTree {
 	 */
 	public TreeNode insert(int value) {
 		TreeNode newNode = new TreeNode(value);
-		TreeNode returnRoot = root;
 		if (root == null) {
 			root = newNode;
 			return root;
 		}
 			//find the spot to add the node
+		TreeNode returnRoot = root;
 		TreeNode pre = null;
-		while (root != null) {
-			if (root.getValue() > value) {
-				pre = root;
-				root = root.getLeft();
+		TreeNode cur = root;
+		while (cur != null) {
+			pre = cur;
+			if (cur.getValue() > value) {				
+				cur = cur.getLeft();
 			} else {
-				pre = root;
-				root = root.getRight();
+				cur = cur.getRight();
 			}
 		}
-			//now add the node to the correct slot
-			if (pre.getValue() > value) {
-				pre.setLeft(newNode);
-			} else if(pre.getValue() < value) {
-				pre.setRight(newNode);
-			}				
+		//now add the node to the correct slot
+		if (pre.getValue() > value) {
+			pre.setLeft(newNode);
+		} else if(pre.getValue() < value) {
+			pre.setRight(newNode);
+		}				
 		return returnRoot;
 	}
 
@@ -47,34 +46,14 @@ public class MyBinarySearchTree {
 	 * @return TreeNode the target TreeNode, null if not exist
 	 */
 	public TreeNode search(int target) {
-		if (target == root.getValue()) {
-			return root;
-		} else if (target > root.getValue()) {
-			// start find the right of the root
-			root = root.getRight();
-			while (root != null) {
-				if (root.getValue() == target) {
-					return root;
-				} else if (root.getValue() > target) {
-					root = root.getLeft();
-				} else {
-					root = root.getRight();
-				}
-			}
-		} else {
-			// start find the left of the root
-			root = root.getLeft();
-			while (root != null) {
-				if (root.getValue() == target) {
-					return root;
-				} else if (root.getValue() < target) {
-					root = root.getRight();
-				} else {
-					root = root.getLeft();
-				}
-			}
+		while (root != null && root.getValue() != target) {
+			if (root.getValue() < target) {
+				root = root.getRight();
+			} else {
+				root = root.getLeft();
+			}		
 		}
-		return null;
+		return root;
 	}
 
 	/**
@@ -85,6 +64,9 @@ public class MyBinarySearchTree {
 	 * @return TreeNode the root of the BST tree after deletion
 	 */
 	public TreeNode delete(int value) {
+		
+		
+		
 		return null;
 	}
 }
--- javabasic/projects/javacollections-app/src/test/java/com/mycompany/app/MyBinarySearchTreeTest.java
+++ javabasic/projects/javacollections-app/src/test/java/com/mycompany/app/MyBinarySearchTreeTest.java
@@ -14,19 +14,23 @@ public class MyBinarySearchTreeTest {
 	public void testInsert() {	
 		System.out.println("TEST Insert ");
 		MyBinarySearchTree tree = new MyBinarySearchTree();
+
 		tree.insert(10);
 		tree.insert(5);
 		tree.insert(15);	
 		tree.insert(4);
 		tree.insert(8);
 		tree.insert(6);	
-		tree.insert(9);		
+		tree.insert(9);	
 		tree.insert(16);	
 		tree.insert(14);
-		TreeNode result = tree.insert(11);
-		
+		TreeNode result = tree.insert(11);		
 		int resultVal = result.getValue();
 		System.out.println("result = " + resultVal);
+		int left = result.getLeft().getValue();
+		int right = result.getRight().getValue();
+		assertEquals(5, left);	
+		assertEquals(15, right);	
 		assertEquals(10, resultVal);		
 	}
 	
@@ -35,11 +39,13 @@ public class MyBinarySearchTreeTest {
 		System.out.println("TEST Search ");
 		MyBinarySearchTree tree = new MyBinarySearchTree();
 		tree.insert(1);
-		tree.insert(3);
-		tree.insert(2);		
+//		tree.insert(3);
+//		tree.insert(2);		
 		TreeNode result = tree.search(1);
 		int resultVal = result.getValue();
 		System.out.println("result = " + resultVal);
 		assertEquals(1, resultVal);		
 	}
+	
+	
 }
