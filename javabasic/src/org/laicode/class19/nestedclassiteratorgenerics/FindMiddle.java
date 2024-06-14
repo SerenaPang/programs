eeb94e52ed052b47bf4869a858d7003e8af05abc
SerenaPang4 <ypang4@usfca.edu>
2024-06-13 18:43:35 -0700

    Example of Generics

--- javabasic/src/org/laicode/class19/nestedclassiteratorgenerics/FindMiddle.java
+++ javabasic/src/org/laicode/class19/nestedclassiteratorgenerics/FindMiddle.java
@@ -3,6 +3,8 @@ package org.laicode.class19.nestedclassiteratorgenerics;
 /**
  * Examples of Generics
  */
+
+//find the middle element in the array
 public class FindMiddle {
 	public static <E> E findMiddle(E[] array) {
 		E midElement = null;
@@ -11,7 +13,21 @@ public class FindMiddle {
 		return midElement;
 	}
 
+	// find the minimum element in the array
+	public static <E extends Comparable<E>> E getMin(E[] arr) {
+		E min = null;
+		if (arr == null || arr.length == 0) {
+			return null;
+		}
+		min = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			min = arr[i].compareTo(min) < 0 ? arr[i] : min;
+		}
+		return min;
+	}
+
 	public static void main(String[] args) {
+		// test find middle element example
 		String[] strArray = { "Silver", "Diana", "Babycat" };
 		Integer[] intArr = { 1, 2, 3 };
 
@@ -20,5 +36,15 @@ public class FindMiddle {
 
 		System.out.println("String mid: " + strMid);
 		System.out.println("Integer mid: " + intMid);
+
+		// test find minimum element example
+		Double[] doubleArr = { 1.1, 1.2, 1.3, 3.3, 4.4 };
+		Character[] charArr = { 'W', 'O', 'R', 'L', 'D' };
+		Integer m1 = getMin(intArr);
+		Double m2 = getMin(doubleArr);
+		Character m3 = getMin(charArr);
+		System.out.println("intArr min: " + m1);
+		System.out.println("doubleArr min: " + m2);
+		System.out.println("charArr min: " + m3);
 	}
 }
